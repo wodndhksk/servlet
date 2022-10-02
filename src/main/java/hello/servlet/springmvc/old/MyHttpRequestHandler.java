d94a25bd75efb573eb3ee6ca66272276b12be2f3
Jaewoo Kim <wodndhksk@gmail.com>
2022-10-02 18:23:05 +0900

    핸들러 매핑과 핸들러 어댑터

---
+++ src/main/java/hello/servlet/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,22 @@
+package hello.servlet.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * MyHttpRequestHandler 를 실행하면서 사용된 객체는 다음과 같다.
+ * HandlerMapping = BeanNameUrlHandlerMapping
+ * HandlerAdapter = HttpRequestHandlerAdapter
+ */
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
---
+++ src/main/java/hello/servlet/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * OldController 를 실행하면서 사용된 객체는 다음과 같다.
+ * HandlerMapping = BeanNameUrlHandlerMapping
+ * HandlerAdapter = SimpleControllerHandlerAdapter
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
